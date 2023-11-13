e6f77d3f91019fe656479d5d9157495bb7f6db26
AJ Abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-11-13 16:59:13 +0500

    Config source-mapping in Webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,8 +4,9 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
+    devtool: "inline-source-map",
     devServer: {
         static: ['./dist'],
         watchFiles: ['./src/**/*'], // to serve changes to HTML
-        }
+        },
 });
