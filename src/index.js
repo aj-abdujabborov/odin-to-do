356c305f9ca9de943168649d9f33903e19337b4b
AJ Abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-11-15 22:19:27 +0500

    Make all todo item buttons function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,37 @@ import TodoList from './scripts/TodoList.js'
 import ListManager from "./scripts/ListManager.js";
 import storageManager from "./scripts/StorageManager.js";
 
+const critNodes = (function CriticalNodes() {
+    const page = {
+        projects: document.querySelector("div.navbar div.projects"),
+        main: document.querySelector("div.main"),
+        todos: document.querySelector("div.todos-container"),
+    }
+
+    const templProj = (function(){
+        const node = document.querySelector("template#project-template").content;
+        const name = node.querySelector("button.project-button");
+        return {node, name};
+    })();
+
+    const templTodo = (function(){
+        const node = document.querySelector("template#todo-template").content;
+        const todo = node.querySelector("div.todo");
+        const check = node.querySelector("button.check span");
+        const title = node.querySelector("p.todo-title");
+        const description = node.querySelector("p.todo-description");
+        const date = node.querySelector("span.date");
+        const project = node.querySelector("span.project");
+        const moveUp = node.querySelector("#move-up");
+        const moveDown = node.querySelector("#move-down");
+        const deleteButton = node.querySelector("#delete-task");
+
+        return {node, todo, check, title, description, date, project, moveUp, moveDown, deleteButton}
+    })();
+
+    return {page, templProj, templTodo};
+})();
+
 const LM = new ListManager();
 
 if (localStorage.length === 0) {
@@ -18,43 +49,35 @@ if (localStorage.length === 0) {
     LM.getList("General").addTodo(new Todo("Pick up kids", "", new Date(2023, 11, 12)), 1);
     LM.getListNames();
     storageManager.save(LM);
+    DOMManager('General');
 }
 else {
     storageManager.load(LM);
+    DOMManager('General'); // 
 }
 
-const critNodes = (function CriticalNodes() {
-    const page = {
-        projects: document.querySelector("div.navbar div.projects"),
-        main: document.querySelector("div.main"),
-    }
-
-    const templProj = (function(){
-        const node = document.querySelector("template#project-template").content;
-        const name = node.querySelector("button.project-button");
-        return {node, name};
-    })();
-
-    const templTodo = (function(){
-        const node = document.querySelector("template#todo-template").content;
-        const check = node.querySelector("button.check");
-        const title = node.querySelector("p.todo-title");
-        const description = node.querySelector("p.todo-description");
-        const date = node.querySelector("span.date");
-        const project = node.querySelector("span.project");
-        const moveUp = node.querySelector("#move-up");
-        const moveDown = node.querySelector("#move-down");
-        const deleteButton = node.querySelector("#delete-task");
-
-        return {node, check, title, description, date, project, moveUp, moveDown, deleteButton}
-    })();
-
-    return {page, templProj, templTodo};
-})();
 
 
-(function DOMManager() {
+function DOMManager(projName) {
     const {page, templProj, templTodo} = critNodes;
+
+    const todoActions = {
+        "move-up": (elem, parElem) => {
+            LM.getList(projName).moveTodoUp(Number(parElem.dataset.index));
+        },
+        "move-down": (elem, parElem) => {
+            LM.getList(projName).moveTodoDown(Number(parElem.dataset.index));
+        },
+        "delete-task": (elem, parElem) => {
+            LM.getList(projName).removeTodo(Number(parElem.dataset.index));
+        },
+        "check": (elem, parElem) => {
+            LM.getList(projName).getTodo(Number(parElem.dataset.index)).toggleStatus();
+        },
+        "edit": (elem, parElem) => {
+
+        },
+    };
 
     function populateNavbar() {
         page.projects.innerText = "";
@@ -66,31 +89,67 @@ const critNodes = (function CriticalNodes() {
         })
     }
 
-    function populateTodosOfProject(projectName) {
-        page.main.innerText = "";
-        LM.getList(projectName).getAllTodos().forEach( (elem) => {
-            templTodo.title.textContent = elem.getTitle();
-            templTodo.description.textContent = elem.getDescription();
-            templTodo.date.textContent = elem.getDate().toDateString();
-            templTodo.project.textContent = projectName;
-
-            const currTodo = templTodo.node.cloneNode(true);
-            page.main.appendChild(currTodo);
-        })
-    }
-
     function makeProjectButtonsClickable() {
         page.projects.addEventListener("click", (e) => {
             if (!(e.target.className == "project-button")) return;
-            console.log(e);
             populateTodosOfProject(e.target.textContent);
         })
     }
 
+    function populateTodosOfProject(projectName) {
+        projName = projectName; // TODO: getting spaghetti-ish. fix.
+
+        page.todos.innerText = "";
+        LM.getList(projectName).getAllTodos().forEach( (elem, ind) => {
+            templTodo.check.textContent = elem.getStatus() === true ? "check" : "";
+            templTodo.title.textContent = elem.getTitle();
+            templTodo.description.textContent = elem.getDescription();
+            templTodo.date.textContent = elem.getDate().toDateString();
+            templTodo.project.textContent = projectName;
+            templTodo.todo.dataset.index = ind;
+
+            const currTodo = templTodo.todo.cloneNode(true);
+            page.todos.appendChild(currTodo);
+        })
+    }
+
+    function makeTodosClickable() {
+        page.todos.addEventListener("click", (e) => {
+            const elem = e.target.closest("button, div.todo");
+            const parElem = e.target.closest("div.todo");
+
+            if (elem.dataset.action == undefined) return;
+            if (!(elem.dataset.action in todoActions)) {
+                throw Error("The element has an action attribute but no programmed action");
+                return;
+            }
+
+            todoActions[elem.dataset.action](elem, parElem);
+            populateTodosOfProject(projName);
+        })
+
+        // function searchForElementTypeUntilParent(startElem, searchSelector, parentSelector) {
+        //     if (startElem.matches(parentSelector)) return startElem;
+        //     if (startElem.matches("body")) return false;
+
+        //     if (startElem.matches(searchSelector)) {
+        //         return startElem;
+        //     }
+        //     else {
+        //         return searchForElementTypeUntilParent(startElem.parentNode, searchSelector, parentSelector);
+        //     }
+        // }
+    }
+
     populateNavbar();
-    populateTodosOfProject('General');
     makeProjectButtonsClickable();
-})();
+    populateTodosOfProject(projName);
+    makeTodosClickable();
+};
+
+
+
+
 
 // storageManager.load(LM);
 
